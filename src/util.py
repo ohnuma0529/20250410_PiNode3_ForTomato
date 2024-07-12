b74967b455e9f120a3726e27488ac3b08b2d73b6
kurita32 <kurita.kazuaki.21@gmail.com>
2024-07-12 14:09:51 +0900

    add : util and usb 説明

--- src/util.py
+++ src/util.py
@@ -3,6 +3,14 @@ from pathlib import Path
 
 
 def get_pinode_config(config_path="/home/pinode3/config.json"):
+    """
+    Pinodeの基本情報を取得 
+
+    [config.json](https://github.com/MinenoLab/PiNode3/blob/e3983b7810d5797727f4daf57e4e7f7a659df84a/config.json)
+
+    Returns:
+    pinode_config(json_object): config.jsonに含まれるPinode基礎情報
+    """
     if Path(config_path).is_file():
         with open(config_path) as f:
             pinode_config = json.load(f)
