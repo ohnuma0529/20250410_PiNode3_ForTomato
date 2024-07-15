e10499d1ec78a3ffd21d550fbaefb7d1e1de08d4
kurita32 <kurita.kazuaki.21@gmail.com>
2024-07-16 08:49:39 +0900

    add : sensor.py 説明

--- src/sensor.py
+++ src/sensor.py
@@ -13,19 +13,71 @@ import util
 from db import InfluxDB
 
 class SensorResult(Enum):
-    SUCCESS             = 0  # 正常
-    EMPTY_STRING_ERROR  = 1  # 空文字エラー
-    NAN_ERROR           = 2  # nanエラー
-    INF_ERROR           = 3  # infエラー
-    MIN_VALUE_ERROR     = 4  # 最小値エラー
-    MAX_VALUE_ERROR     = 5  # 最大値エラー
+    """
+    数字を定数として扱うためにconstantsを作成
+
+    constants:
+    SUCCESS             = 0  : 正常
+    EMPTY_STRING_ERROR  = 1  : 空文字エラー
+    NAN_ERROR           = 2  : nanエラー
+    INF_ERROR           = 3  : infエラー
+    MIN_VALUE_ERROR     = 4  : 最小値エラー
+    MAX_VALUE_ERROR     = 5  : 最大値エラー
+
+    """
+    SUCCESS             = 0  
+    EMPTY_STRING_ERROR  = 1  
+    NAN_ERROR           = 2  
+    INF_ERROR           = 3  
+    MIN_VALUE_ERROR     = 4  
+    MAX_VALUE_ERROR     = 5  
 
 class MCP3204:
+    """
+    MPC3204 ADC(アナログデジタルコンバータ)を使用するためのクラス
+    
+    paramerters:
+    spi: SPIインターフェースオブジェクト
+    vref: 参照電圧(今回の場合は5.0V)
+
+    """
     def __init__(self, spi, vref=5.0):
         self.spi = spi
         self.vref = vref
 
     def read(self, ch):
+        """
+        指定したチャンネルからアナログ値を読み取りデジタル値に変換後，電圧に変換して返す
+
+        Parameter:
+        ch: チャンネル番号 (0～3)
+        Return:
+        チャンネルの電圧値(ボルト単位),チャンネル番号が無効な場合-1
+
+        Details:
+        MCP3204の通信フォーマットではSPI通信を通じて3バイトのデータでの送信が行われる.
+        
+        xfer2([0x06, data2, 0x00])では通信フォーマットに従って以下の3バイトのデータを指定する
+        1番目のバイト: '0x06' スタートビットとモードビット(今回はシングルエンドモード)
+                       スタートビット: MCP3204に対して通信の開始を示すビット
+                       モードビット: MCP3204がどのモードでデータを取得するかを示すビット.今回はシングルエンドモード
+        2番目のバイト: data2  チャネル番号 
+                      '0bCCCC0000'のようなフォーマットで指定する. CCCC部分にチャネル番号を指定
+                      CCCC部分をチャネル番号にするため (ch << 6) & 0xff を実行してchをシフト&整形
+        3番目のバイト: '0x00' ダミーバイト
+                       このバイトはMCP3204からの応答データを完全に受信するために必要である.
+                       今回個のバイトで送るデータはないが通信サイクルを成立させるために適当なダミーデータを指定
+
+        通信によって返却された3バイトの値がretに格納される
+        ret (list): ret[0]: 1番目のバイト: 最初の送信データに対するMCP3204からの応答. (一般的には無視される)  
+                    ret[1]: 2番目のバイト: 下位4ビットにデータが含まれる
+                    ret[2]: 3番目のバイト: 8ビットすべてにデータが含まれる
+        
+        raw = ((ret[1] & 0x0f) << 8) + ret[2]ではret[1]とret[2]の意味を持つバイトを12ビットとして取り出す.
+
+        volts = raw * self.vref / 4096.0 :ADCが生成する12ビットのデジタル値がアナログ入力電圧にどのように対応するかを計算
+                                          4096(2^12)で割ることでデジタル値を電圧値にスケーリングして正確な電圧値を取得
+        """
         if ch < 0 or ch > 4:
             return -1
         data2 = (ch << 6) & 0xff
@@ -35,7 +87,23 @@ class MCP3204:
         return volts
 
 class Sensor:
+    """
+    センサからの情報取得を行う        
+    """
     def __init__(self):
+        """
+        センサクラスの初期化を行うメソッド
+        Details:
+
+        センサデータ読み込みのためにGPIOの初期化, pigpioデーモンとの接続を作成(グローバル変数 pi)
+        各種設定をconfig.jsonから取得
+
+        前回取得したセンサデータがprevious_sensor_data.jsonに記録されている
+        このデータを取得しself.previous_sensor_data.jsonに格納        
+        [previous_sensor_data.json] https://github.com/MinenoLab/PiNode3/blob/815bc1191299f1e08c01693903b32b1550b43e7c/src/previous_sensor_data.json
+
+        I2CセンサのコマンドとSPIチャンネルのリストを取得
+        """
         read_sensor.init_pigpio()
         self.config = util.get_pinode_config()
 
@@ -47,6 +115,13 @@ class Sensor:
         self.spi_sensor_list = list(self.config['sensor']['spi_channel'].keys())
     
     def get(self, sensor:str):
+        """
+        各種センサデータを取得するためのメソッド
+        センサごとに指定回数だけデータ取得を試みる(デフォルトではすべてのセンサで3回試行)
+        
+        # TODO
+
+        """
         for _ in range(self.config['sensor']['max_retry_count'][sensor]):
             try:
                 data = read_sensor.get(sensor)
